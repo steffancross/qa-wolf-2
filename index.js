1476fe68416f9ed3c53b06cebb0f79ed5ad1573a
Steffan Cross <steffancross@berkeley.edu>
2024-07-09 22:28:35 -0700

    some refactoring

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { loadNextPage, validateSequentialTime } = require("./helperFunctions");
 const moment = require("moment");
 
 // More understandable approach, get all articles first, then check if they are correctly ordered.
+// Two passes, data collection and data validation.
 async function sortHackerNewsArticles() {
   // launch browser
   const browser = await chromium.launch({ headless: false });
@@ -29,8 +30,8 @@ async function sortHackerNewsArticles() {
     : console.log("Articles were not ordered newest to oldest");
 }
 
-// Less straight forward but more time and space efficient approach.
-// One pass through and no extra memory.
+// Less straight forward but more time and space efficient.
+// One pass, no extra memory, and can early exit.
 async function sortHackerNewsArticles2() {
   // launch browser
   const browser = await chromium.launch({ headless: false });
@@ -40,37 +41,40 @@ async function sortHackerNewsArticles2() {
   // go to Hacker News
   await page.goto("https://news.ycombinator.com/newest");
 
-  let isValid = true;
-  let previousTime = null;
+  let isOrdered = true;
+  let prevArtcTime = null;
   let articlesChecked = 0;
   let currentPageIndex = 0;
 
   while (articlesChecked < 100) {
-    const currentAge = await page.locator(".age").nth(currentPageIndex);
-    const currentTime = await currentAge.getAttribute("title");
+    const currentElement = await page.locator(".age").nth(currentPageIndex);
+    const currentArtcTime = await currentElement.getAttribute("title");
 
-    if (previousTime && moment(currentTime).isAfter(previousTime)) {
-      isValid = false;
+    if (prevArtcTime && moment(currentArtcTime).isAfter(prevArtcTime)) {
+      isOrdered = false;
       break;
     }
 
-    const lastTime = await page.locator(".age").last().getAttribute("title");
-    if (currentTime === lastTime) {
+    const lastArtcTime = await page
+      .locator(".age")
+      .last()
+      .getAttribute("title");
+    if (currentArtcTime === lastArtcTime) {
       await loadNextPage(page);
       currentPageIndex = 0;
     }
 
-    previousTime = currentTime;
+    prevArtcTime = currentArtcTime;
     articlesChecked++;
     currentPageIndex++;
   }
 
-  isValid
+  isOrdered
     ? console.log("Articles were correctly ordered newest to oldest")
     : console.log("Articles were not ordered newest to oldest");
 }
 
 (async () => {
-  // await sortHackerNewsArticles();
-  await sortHackerNewsArticles2();
+  await sortHackerNewsArticles();
+  // await sortHackerNewsArticles2();
 })();
