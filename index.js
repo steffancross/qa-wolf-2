99439dc30f3dc93274267354301f03acb89d2e6b
Steffan Cross <steffancross@berkeley.edu>
2024-07-09 22:48:23 -0700

    added try catch to functions

--- index.js
+++ index.js
@@ -11,23 +11,29 @@ async function sortHackerNewsArticles() {
   const context = await browser.newContext();
   const page = await context.newPage();
 
-  // go to Hacker News
-  await page.goto("https://news.ycombinator.com/newest");
+  try {
+    // go to Hacker News
+    await page.goto("https://news.ycombinator.com/newest");
 
-  let articlePostTimes = [];
-  while (articlePostTimes.length < 100) {
-    const temporaryLocators = await page.locator(".age").all();
+    let articlePostTimes = [];
+    while (articlePostTimes.length < 100) {
+      const temporaryLocators = await page.locator(".age").all();
 
-    for (const locator of temporaryLocators) {
-      const timestamp = await locator.getAttribute("title");
-      articlePostTimes.push(timestamp);
+      for (const locator of temporaryLocators) {
+        const timestamp = await locator.getAttribute("title");
+        articlePostTimes.push(timestamp);
+      }
+      await loadNextPage(page);
     }
-    await loadNextPage(page);
+    articlePostTimes = articlePostTimes.slice(0, 100);
+    validateSequentialTime(articlePostTimes)
+      ? console.log("Articles were correctly ordered newest to oldest")
+      : console.log("Articles were not ordered newest to oldest");
+  } catch (error) {
+    console.error("error :(", error);
+  } finally {
+    await browser.close();
   }
-  articlePostTimes = articlePostTimes.slice(0, 100);
-  validateSequentialTime(articlePostTimes)
-    ? console.log("Articles were correctly ordered newest to oldest")
-    : console.log("Articles were not ordered newest to oldest");
 }
 
 // Less straight forward but more time and space efficient.
@@ -38,37 +44,43 @@ async function sortHackerNewsArticles2() {
   const context = await browser.newContext();
   const page = await context.newPage();
 
-  // go to Hacker News
-  await page.goto("https://news.ycombinator.com/newest");
+  try {
+    // go to Hacker News
+    await page.goto("https://news.ycombinator.com/newest");
 
-  let isOrdered = true;
-  let prevArtcTime = null;
-  let articlesChecked = 0;
-  let currentPageIndex = 0;
-  const nextPageIndex = (await page.locator(".age").count()) - 1;
+    let isOrdered = true;
+    let prevArtcTime = null;
+    let articlesChecked = 0;
+    let currentPageIndex = 0;
+    const nextPageIndex = (await page.locator(".age").count()) - 1;
 
-  while (articlesChecked < 100) {
-    const currentElement = await page.locator(".age").nth(currentPageIndex);
-    const currentArtcTime = await currentElement.getAttribute("title");
+    while (articlesChecked < 100) {
+      const currentElement = await page.locator(".age").nth(currentPageIndex);
+      const currentArtcTime = await currentElement.getAttribute("title");
 
-    if (prevArtcTime && moment(currentArtcTime).isAfter(prevArtcTime)) {
-      isOrdered = false;
-      break;
+      if (prevArtcTime && moment(currentArtcTime).isAfter(prevArtcTime)) {
+        isOrdered = false;
+        break;
+      }
+
+      if (currentPageIndex === nextPageIndex) {
+        await loadNextPage(page);
+        currentPageIndex = 0;
+      }
+
+      prevArtcTime = currentArtcTime;
+      articlesChecked++;
+      currentPageIndex++;
     }
 
-    if (currentPageIndex === nextPageIndex) {
-      await loadNextPage(page);
-      currentPageIndex = 0;
-    }
-
-    prevArtcTime = currentArtcTime;
-    articlesChecked++;
-    currentPageIndex++;
+    isOrdered
+      ? console.log("Articles were correctly ordered newest to oldest")
+      : console.log("Articles were not ordered newest to oldest");
+  } catch (error) {
+    console.error("error :(", error);
+  } finally {
+    await browser.close();
   }
-
-  isOrdered
-    ? console.log("Articles were correctly ordered newest to oldest")
-    : console.log("Articles were not ordered newest to oldest");
 }
 
 (async () => {
